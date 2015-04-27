e999cda584adce345ae44cf00bc2a233c5dcb204
Jan Sarenik <jasan@apiary.io>
2015-04-27 19:17:52 +0200

    Simplify even more - no intermediate files

--- compute.sh
+++ compute.sh
@@ -1,18 +1,3 @@
 #!/bin/sh
 
-sumit() {
-  paste -sd+ - | bc
-}
-
-processit() {
-  FILE=$1
-  FILE=${FILE%.tsv.gz}
-  gzcat $FILE.tsv.gz | grep -Eo 'bytes=[0-9]+' | cut -d= -f2 > $FILE.bytes
-}
-
-for file in *.tsv.gz
-do
-  processit $file
-done
-
-cat *.bytes | sumit
+gzcat *.tsv.gz | grep -Eo 'bytes=[0-9]+' | cut -d= -f2 | paste -sd+ - | bc
