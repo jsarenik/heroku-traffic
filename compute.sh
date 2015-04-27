07a4827e6b9436208ca909aa15f873063a1e3187
Jan Sarenik <jasan@apiary.io>
2015-04-27 19:13:24 +0200

    Simplify the summing

--- compute.sh
+++ compute.sh
@@ -8,12 +8,11 @@ processit() {
   FILE=$1
   FILE=${FILE%.tsv.gz}
   gzcat $FILE.tsv.gz | grep -Eo 'bytes=[0-9]+' | cut -d= -f2 > $FILE.bytes
-  cat $FILE.bytes | sumit > $FILE.sum
 }
 
 for file in *.tsv.gz
 do
-  process $file
+  processit $file
 done
 
-cat *.sum | sumit
+cat *.bytes | sumit
